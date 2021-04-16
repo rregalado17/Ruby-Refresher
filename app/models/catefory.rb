c8ddc276a58bb01fb59aca66493309d0fce835c6
rregalado17 <rregalado17@gmail.com>
2021-04-16 00:44:40 -0400

    update views

--- app/models/catefory.rb
+++ app/models/catefory.rb
@@ -1,2 +1,3 @@
 class Catefory < ApplicationRecord
+    validates_presence_of :title, :url
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :summary, presence: true, length: { maximum: 200 }
+    validates :body, presence: true, length: {minimum: 100 }
+
+    def details
+        "This post was created on #{ created_at.strftime("%d %M %Y") }"
+    end
+
+    def self.total
+        all.size
+    end
+
 end
